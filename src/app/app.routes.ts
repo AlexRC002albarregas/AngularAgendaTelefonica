9c3bddd4c484f7cc0f7585cf61bb0bc8ef62f8c5
Alejandro <areinac01@iesalbarregas.es>
2024-05-27 11:53:27 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { ContactListComponent } from './contact-list/contact-list.component';
 
 export const routes: Routes = [
-    { path: "contact", component: ContactListComponent }
+    { path: '', component: ContactListComponent }
 ];
